71e64ac72a50745ba2b11ff47af3a22c9ef2982e
personal <personal@USER>
2025-07-26 16:10:21 +0300

    Add project files.

---
+++ UniK/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniK.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
