f5bb2c421becb2004d200b968dfcf2b338064ab6
personal <kossev89@gmail.com>
2025-07-28 00:01:32 +0300

    Added several constants

--- UniK.Infrastructure/Constants/DataConstants.cs
+++ UniK.Infrastructure/Constants/DataConstants.cs
@@ -8,5 +8,26 @@ namespace UniK.Infrastructure.Constants
 {
     public static class DataConstants
     {
+        public const int MaxModelLength = 100;
+        public const int MaxConstructionLength = 200;
+        public const int MaxProductivityLength = 200;
+        public const int MaxOverallDimensionsLength = 200;
+        public const int MaxWeightLength = 100;
+        public const int MaxInterfaceLength = 100;
+        public const int MaxDrivingLength = 100;
+        public const int MaxElectricalSupplyLength = 100;
+        public const int MaxInstalledPowerLength = 100;
+        public const int MaxCompressedAirConsumptionLength = 100;
+        public const int MaxElectronicsLength = 200;
+        // ProductFeature
+        public const int MaxFeatureNameLength = 100;
+        public const int MaxFeatureValueLength = 200;
+        // PackagingMachine
+        public const int MaxFilmWidthLength = 100;
+        public const int MaxFilmThicknessLength = 100;
+        public const int MaxBagLengthLength = 100;
+        public const int MaxFilmMaterialLength = 100;
+        public const int MaxImagePathLength = 500;
+
     }
 }
